a174c417a60dd476cdd1a0bd0ec3e0fb832a50dc
Valentin Bogdanov <valentin@clippings.com>
2019-06-20 17:56:28 +0300

    Export path to PHP's bin folder once only

--- brew.sh
+++ brew.sh
@@ -16,6 +16,6 @@ pecl install xdebug
 # Cleanup old downloads
 brew cleanup
 
-# Link to homebrew's php
-echo 'export PATH="/usr/local/opt/php@7.1/bin:$PATH"' >> ~/.bash_profile
-echo 'export PATH="/usr/local/opt/php@7.1/sbin:$PATH"' >> ~/.bash_profile
+# If needed override the systems' PHP with the version from the Brewfile (currently version 7.1)
+grep 'export PATH="/usr/local/opt/php@7.1/bin:$PATH"' ~/.bash_profile || echo 'export PATH="/usr/local/opt/php@7.1/bin:$PATH"' >> ~/.bash_profile
+grep 'export PATH="/usr/local/opt/php@7.1/sbin:$PATH"' ~/.bash_profile || echo 'export PATH="/usr/local/opt/php@7.1/sbin:$PATH"' >> ~/.bash_profile
