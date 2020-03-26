b7a80ed43af87a0c105b1c1d2477133204ef2765
BaselAl <basel@clippings.com>
2020-03-26 21:18:16 +0200

    add_missing_brew_sh

--- brew.sh
+++ brew.sh
@@ -3,15 +3,30 @@
 # Install Brew if not installed
 command -v brew > /dev/null || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Update Brew and Formulae
+# Update Brew and Formula
 brew update
 
 # Install brew bundle
 brew tap Homebrew/bundle
 
-# Install tools and apps from the Brewfile wih Brew and Cask
-brew bundle --file=Brewfile
-pecl install xdebug
+# Install tools and apps for Devolepers or Non-Developers
+echo "Type the job role ( Developer or Non-Developer ), followed by [ENTER]:"
+read role
+
+while [[ "$role" != "Developer" && "$role" != "Non-Developer" ]]
+do
+  echo "Please type the correct format"
+  read role
+done
+
+if [ "$role" != "Non-Developer" ]
+then
+  brew bundle --file=Developer
+  pecl install xdebug
+else
+  brew bundle --file=Non-Developer
+  pecl install xdebug
+fi
 
 # Cleanup old downloads
 brew cleanup
